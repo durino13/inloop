0aece10cbf08ba8fc45072be0b994357a7e13fb6
Juraj <juraj.marusiak@gmail.com>
2017-05-22 21:48:57 +0200

    Production build changed, added decorators support ..

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -124,7 +124,8 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+        cacheDirectory: true,
+        plugins: ['transform-decorators-legacy' ]
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
